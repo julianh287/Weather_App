d3856ba8a469f44ac8ce633c8dfefd6c655815a8
JulianH <julian.her.arg@gmail.com>
2021-07-07 23:03:47 -0300

    uploading proyect

---
+++ src/components/About.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function About (props) {
+    return (
+        <div>
+            <h2>Henry Weather App</h2>
+
+        <p>Esta es una aplicación que permite saber el clima en tiempo real de cualquier ciudad del mundo. </p>
+        <p>Para eso solo tenemos que introducir el nombre de la ciudad que deseamos buscar y la aplicación nos traerá una tarjeta con la información del tiempo atmosférico en esa ciudad.</p>
+        <p>Para más información podemos hacer click en la tarjeta.</p>
+        </div>
+    )
+}
